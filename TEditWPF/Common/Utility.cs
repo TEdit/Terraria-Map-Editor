83d0ec0b1753ab2f60f43f71cc8680e8b027d287
BinaryConstruct <binaryconstruct@gmail.com>
2011-06-09 20:54:08 -0500

    Adding WPF test project, attempting to improve performance

---
+++ TEditWPF/Common/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TerrariaMapEditor.Common
+{
+    public static class Utility
+    {
+        public static bool IsNumeric(string val, System.Globalization.NumberStyles numberStyle)
+        {
+            Int32 result;
+            return Int32.TryParse(val, numberStyle,
+                System.Globalization.CultureInfo.CurrentCulture, out result);
+        }
+    }
+}
