2a11ded068c0d34580356a855214a078bbfeeea1
BinaryConstruct <binaryconstruct@gmail.com>
2011-08-17 23:05:24 -0500

    Version updated to 1.0.8.4 (Terraria 1.0.6.1)

--- TEdit/Properties/AssemblyInfo.cs
+++ TEdit/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.8.3")]
-[assembly: AssemblyFileVersion("2.0.8.3")]
+[assembly: AssemblyVersion("2.0.8.4")]
+[assembly: AssemblyFileVersion("2.0.8.4")]
