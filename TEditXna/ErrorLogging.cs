03b9e4cfba524109fac9de40e8572464be71739f
BinaryConstruct <binaryconstruct@gmail.com>
2012-01-09 10:36:36 -0600

    Adding file version to error log

--- TEditXna/ErrorLogging.cs
+++ TEditXna/ErrorLogging.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 
 namespace TEditXna
 {
@@ -21,8 +23,11 @@ namespace TEditXna
         public static void Log(string message)
         {
             File.AppendAllText("log.txt",
-                               DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + ": " +
-                               message + Environment.NewLine);
+                               string.Format("{0} v{1} {2} {3}",
+                               DateTime.Now.ToString("MM-dd-yyyy HH:mm"),
+                               Version,
+                               message,
+                               Environment.NewLine));
         }
 
         public static void LogException(object ex)
@@ -40,6 +45,16 @@ namespace TEditXna
                     LogException(e.InnerException);
 
                 Log(String.Format("{0} - {1}\r\n{2}", ErrorLevel.Error, e.Message, e.StackTrace));
+            }
+        }
+
+        public static string Version
+        {
+            get
+            {
+                Assembly asm = Assembly.GetExecutingAssembly();
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(asm.Location);
+                return fvi.FileVersion;
             }
         }
     }
