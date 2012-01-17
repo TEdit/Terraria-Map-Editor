21a46042db1797c3f7a509208f35d642057336fc
BinaryConstruct <binaryconstruct@gmail.com>
2012-01-16 23:16:44 -0600

    Updating version

--- TEditXna/Properties/AssemblyInfo.cs
+++ TEditXna/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.12009.2302")]
+[assembly: AssemblyFileVersion("3.1.12016.2307")]
