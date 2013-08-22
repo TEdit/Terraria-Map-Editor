34182da24620a3a6991e80777730c64f140c6d28
BinaryConstruct <binaryconstruct@gmail.com>
2013-08-22 11:54:39 -0500

    update build

--- TEditXna/Properties/AssemblyInfo.cs
+++ TEditXna/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.12322.1200")]
+[assembly: AssemblyFileVersion("3.3.13234.1153")]
