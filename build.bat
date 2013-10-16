b91fbb6f76f96d9608ffe9616a19107a4417f762
BinaryConstruct <binaryconstruct@gmail.com>
2013-10-16 01:20:06 -0500

    modify build script a little

--- build.bat
+++ build.bat
@@ -1,11 +1,12 @@
 @echo off
 
 rmdir /S /Q .\BIN
-mkdir .\BIN
+
 
 echo Building Solution...
 set msbuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 %msbuild% .\Build.proj /property:Configuration=Release;Platform=x86 
 
 echo Copying Files...
+mkdir .\BIN
 if exist ".\TEdit3Installer\bin\Release\TEdit3Installer.msi" copy ".\TEdit3Installer\bin\Release\TEdit3Installer.msi" .\BIN\
