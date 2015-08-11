e82bd67605f62bee04b4ca241d15f208a1d0a632
BinaryConstruct <binaryconstruct@gmail.com>
2015-08-11 10:15:27 -0500

    Update nightly.bat

--- nightly.bat
+++ nightly.bat
@@ -2,7 +2,7 @@
 
 echo Building Solution...
 set msbuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-%msbuild% .\TEditNoInstaller.sln /property:Configuration=Release;Platform="Any CPU"
+%msbuild% .\TEdit.sln /property:Configuration=Release;Platform="Any CPU"
 
 REM echo Copying Files...
 REM if exist ".\TEdit3Installer\bin\Release\TEdit3Installer.msi" copy ".\TEdit3Installer\bin\Release\TEdit3Installer.msi" .\BIN\
