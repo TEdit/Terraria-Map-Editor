c30d2299480685524f6a27e49c30cfabc3626612
BinaryConstruct <binaryconstruct@gmail.com>
2015-08-10 23:37:12 -0500

    update build

--- package.ps1
+++ package.ps1
@@ -2,6 +2,11 @@ $source = ".\TEditXna\bin\Release"
 $version = Get-Content .\teditversion.txt
 $destination = ".\build\TEdit3_BETA_$version.zip"
 
+if (!(Test-Path -path .\build))
+{
+    New-Item .\build -type directory
+}
+
 Write-Host "Zipping $source ..." -ForegroundColor Yellow
 
  If(Test-path $destination) {Remove-item $destination}
