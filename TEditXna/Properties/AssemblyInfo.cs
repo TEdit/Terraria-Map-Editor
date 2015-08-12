600db0d5e1c8f245e06d04d61fa9357d91774901
BinaryConstruct <binaryconstruct@gmail.com>
2015-08-12 01:20:06 -0500

    Fix AssemblyInfo File

--- TEditXna/Properties/AssemblyInfo.cs
+++ TEditXna/Properties/AssemblyInfo.cs
@@ -1,20 +1,16 @@
-using System.Reflection;
-using System.Resources;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-using System.Windows.Markup;
-
-[assembly: AssemblyTitle("TEditXna")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TEdit")]
+[assembly: AssemblyDescription("Terraria Map Editor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TEditXna")]
-[assembly: AssemblyCopyright("Copyright © BinaryConstruct 2012")]
+[assembly: AssemblyProduct("TEdit")]
+[assembly: AssemblyCopyright("Copyright BinaryConstruct © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,23 +19,17 @@ using System.Windows.Markup;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("9d968d08-c3ec-4d94-b3ed-f28a9123c789")]
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page, 
-                                     // or application resource dictionaries)
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page, 
-                                              // app, or any theme specific resource dictionaries)
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
 )]
 
 
@@ -50,13 +40,9 @@ using System.Windows.Markup;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.6.0.0")]
-[assembly: AssemblyFileVersion("3.6.150811.1028")]
+[assembly: AssemblyFileVersion("3.6.150812.0114")]
 
 [assembly: XmlnsDefinition("http://tedit/wpf", "TEdit.UI.Xaml")]
 [assembly: XmlnsDefinition("http://tedit/wpf", "TEdit.UI.Xaml.Enum")]
 [assembly: XmlnsPrefix("http://tedit/wpf", "tedit")]
-
