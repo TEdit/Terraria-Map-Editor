82d43583e26aa55ca7496eefe6c9c9d3a7fcbbe0
BinaryConstruct <binaryconstruct@gmail.com>
2015-08-12 02:06:35 -0500

    update ftp script

--- ftp.ps1
+++ ftp.ps1
@@ -36,6 +36,32 @@ foreach ($LocalFile in $files)
     Write-Host "FTP done."
 }
 
+Set-Location ..\
+$files = Get-ChildItem .\ -Filter teditversion.txt
+
+foreach ($LocalFile in $files)
+{
+    $RemoteFile = $Server + "/" + [System.IO.Path]::GetFileName($LocalFile)
+    Write-Host "Uploading $LocalFile to $RemoteFile..."
+     # Create FTP Rquest Object
+    $FTPRequest = [System.Net.FtpWebRequest]::Create("$RemoteFile")
+    $FTPRequest = [System.Net.FtpWebRequest]$FTPRequest
+    $FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+    $FTPRequest.Credentials = new-object System.Net.NetworkCredential($Username, $Password)
+    $FTPRequest.UseBinary = $true
+    $FTPRequest.UsePassive = $true
+    # Read the File for Upload
+    $FileContent = gc -en byte $LocalFile
+    $FTPRequest.ContentLength = $FileContent.Length
+    # Get Stream Request by bytes
+    $Run = $FTPRequest.GetRequestStream()
+    $Run.Write($FileContent, 0, $FileContent.Length)
+    # Cleanup
+    $Run.Close()
+    $Run.Dispose()
+    Write-Host "FTP done."
+}
+
  #download
 ## # Create a FTPWebRequest 
 ## $FTPRequest = [System.Net.FtpWebRequest]::Create($RemoteFile) 
