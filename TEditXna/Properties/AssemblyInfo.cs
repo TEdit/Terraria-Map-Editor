bc52f38dd399049961108c3cf33d763e01b48689
BinaryConstruct <binaryconstruct@gmail.com>
2015-08-12 01:21:55 -0500

    Fix AssemblyInfo File

--- TEditXna/Properties/AssemblyInfo.cs
+++ TEditXna/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
