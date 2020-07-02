704fb2e53e6f0a63706830a899024a8f730b181a
binaryconstruct <binaryconstruct@gmail.com>
2020-07-02 15:08:12 -0500

    Misc settings file updater tweaks

--- src/SettingsFileUpdater/Program.cs
+++ src/SettingsFileUpdater/Program.cs
@@ -11,41 +11,77 @@ namespace SettingsFileUpdater
     {
         static void Main(string[] args)
         {
+            //LoadTerrariaAsm();
             RegisterAssemblyResolver();
 
 
             var wrapper = TerrariaHost.TerrariaWrapper.Initialize();
-            (var tilecolors, var wallcolors) = wrapper.GetMapTileColors();
+           
 
             XDocument xdoc = XDocument.Load("settings.xml");
 
             var tiles = xdoc.Root.Element("Tiles");
-
-            foreach (var item in tilecolors)
+            foreach (var tile in tiles.Elements())
             {
-                if (item.Key < 470) continue;
-                var tile = tiles.Elements().FirstOrDefault(t => int.Parse(t.Attribute("Id").Value) == item.Key);
-                tile.SetAttributeValue("Color", item.Value);
+                int id = int.Parse(tile.Attribute("Id").Value);
+                if (Terraria.ID.TileID.Sets.NonSolidSaveSlopes[id])
+                {
+                    tile.SetAttributeValue("SaveSlope", "true");
+                }
+                if (Terraria.Main.tileSolid[id])
+                {
+                    tile.SetAttributeValue("Solid", "true");
+                }
+                if (Terraria.Main.tileSolidTop[id])
+                {
+                    tile.SetAttributeValue("SolidTop", "true");
+                }
             }
-            
-          // var walls = xdoc.Root.Element("Walls");
-          // foreach (var item in wallcolors)
-          // {
-          //     var wall = tiles.Elements().FirstOrDefault(t => int.Parse(t.Attribute("Id").Value) == item.Key);
-          //     wall.SetAttributeValue("Color", item.Value);
-          // }
-
-
-
             xdoc.Save("settings3.xml");
+
+
+            //foreach (var item in tilecolors)
+            //{
+            //    if (item.Key < 470) continue;
+            //    var tile = tiles.Elements().FirstOrDefault(t => int.Parse(t.Attribute("Id").Value) == item.Key);
+            //    tile.SetAttributeValue("Color", item.Value);
+            //}
+
+            // var walls = xdoc.Root.Element("Walls");
+            // foreach (var item in wallcolors)
+            // {
+            //     var wall = tiles.Elements().FirstOrDefault(t => int.Parse(t.Attribute("Id").Value) == item.Key);
+            //     wall.SetAttributeValue("Color", item.Value);
+            // }
+
+
+
 
             //Console.WriteLine(wrapper.GetTilesXml());
             //Console.WriteLine(wrapper.GetWallsXml());
-             Console.WriteLine(wrapper.GetItemsXml());
+            //Console.WriteLine(wrapper.GetItemsXml());
             //Console.WriteLine(wrapper.GetNpcsXml());
             //Console.WriteLine(wrapper.GetPrefixesXml());
 
             Console.ReadLine();
+        }
+
+        private static void LoadTerrariaAsm()
+        {
+            var TerrariaAsm = typeof(Terraria.Program).Assembly;
+            Assembly assembly;
+            foreach (var name in TerrariaAsm.GetManifestResourceNames().Where(n => n.EndsWith(".dll")))
+            {
+                using (Stream manifestResourceStream = TerrariaAsm.GetManifestResourceStream(name))
+                {
+                    byte[] numArray = new byte[manifestResourceStream.Length];
+                    manifestResourceStream.Read(numArray, 0, (int)numArray.Length);
+                    assembly = Assembly.Load(numArray);
+                }
+            }
+
+            var path = Path.GetDirectoryName(TerrariaAsm.Location);
+
         }
 
         private static void RegisterAssemblyResolver()
@@ -66,6 +102,8 @@ namespace SettingsFileUpdater
                     manifestResourceStream.Read(numArray, 0, (int)numArray.Length);
                     assembly = Assembly.Load(numArray);
                 }
+
+
                 return assembly;
             });
         }
