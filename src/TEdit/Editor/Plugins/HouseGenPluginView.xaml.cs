dbab033396b94436141e39889b49b75530a0ac87
Christopher Evans <cge1988@gmail.com>
2021-03-31 17:59:26 -0400

    Added JSON support for loading schematic template data. Added ability to load additional schematics from which to generate from.

--- src/TEdit/Editor/Plugins/HouseGenPluginView.xaml.cs
+++ src/TEdit/Editor/Plugins/HouseGenPluginView.xaml.cs
@@ -1,24 +1,129 @@
 ﻿using System;
 using System.Windows;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.Win32;
 using TEdit.Terraria;
 using TEdit.Editor.Clipboard;
 using TEdit.ViewModel;
-using TEdit.UI.Xaml.XnaContentHost;
-using System.Windows.Media.Imaging;
-using GalaSoft.MvvmLight;
-
-//using Microsoft.Xna.Framework.Graphics;
-
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
 
 namespace TEdit.Editor.Plugins
 {
+    public class HouseGenTemplate
+    {
+        private readonly string _name;
+        private readonly int _schematicId;
+        private readonly HouseGenTemplateData _data;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int SchematicID
+        {
+            get { return _schematicId; }
+        }
+
+        public HouseGenTemplateData Template
+        {
+            get { return _data; }
+        }
+
+        public HouseGenTemplate(int id, string name, HouseGenTemplateData template)
+        {
+            _schematicId = id;
+            _name = name;
+            _data = template;
+        }
+    }
+
+    public class HouseGenTemplateData
+    {
+        public int Count
+        {
+            get;
+            set;
+        }
+
+        public IList <Room> Rooms
+        {
+            get;
+            set;
+        }
+
+        public IList <Roof> Roofs
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Room
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int X
+        {
+            get;
+            set;
+        }
+        public int Y
+        {
+            get;
+            set;
+        }
+        public int Width
+        {
+            get;
+            set;
+        }
+        public int Height
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Roof
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int X
+        {
+            get;
+            set;
+        }
+        public int Y
+        {
+            get;
+            set;
+        }
+        public int Width
+        {
+            get;
+            set;
+        }
+        public int Height
+        {
+            get;
+            set;
+        }
+    }
+
     /// <summary>
-    /// Interaction logic for ReplaceAllPlugin.xaml
+    /// Interaction logic for HouseGenPluginView.xaml
     /// </summary>
     public partial class HouseGenPluginView : Window
     {
-        //private Texture2D _previewTexture;
-
         private WorldViewModel _wvm;
         public WorldViewModel WorldViewModel
         {
@@ -26,199 +131,172 @@ namespace TEdit.Editor.Plugins
             set { _wvm = value; }
         }
 
-        private WriteableBitmap _preview;
-        public WriteableBitmap HouseGenPreview
-        {
-            get { return _preview; }
-        }
-        ClipboardBuffer _template;
-        ClipboardBuffer _buffer;
+        private readonly IList <HouseGenTemplate> HouseGenTemplates;
+        private readonly IList <ClipboardBuffer> HouseGenSchematics;
+
+        private Color _backgroundColor = Color.FromNonPremultiplied(32, 32, 32, 255);
+        
+        ClipboardBuffer _generatedSchematic;
         Geometry.Primitives.Vector2Int32 _bufferSize;
 
         public HouseGenPluginView()
         {
             InitializeComponent();
             _wvm = null;
-            _bufferSize = new(43, 22);
-            LoadTemplateSchematic();
+            _bufferSize = new(0,0);
+            HouseGenTemplates = new List<HouseGenTemplate>();
+            HouseGenSchematics = new List<ClipboardBuffer>();
         }
 
-        private void LoadTemplateSchematic()
+        private bool ImportTemplateSchematic()
         {
-            if (_template == null)
+            try
             {
-                try
+                var ofd = new OpenFileDialog();
+                ofd.Filter = "TEdit House Gen Schematic File|*.TEditHGSch";
+                ofd.DefaultExt = "TEdit House Gen Schematic File|*.TEditHGSch";
+                ofd.Title = "Import TEdit House Gen Schematic File";
+                ofd.InitialDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"My Games\Terraria\Schematics");
+                if (!Directory.Exists(ofd.InitialDirectory))
+                    Directory.CreateDirectory(ofd.InitialDirectory);
+                ofd.Multiselect = false;
+                if ((bool)ofd.ShowDialog())
                 {
-                    _template = ClipboardBuffer.Load("schematics\\HouseGenTemplate.TEditSch");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Schematic File Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
+                    int schematic_id = HouseGenSchematics.Count;
+                    string filename = Path.GetFileNameWithoutExtension(ofd.FileName);
 
-                if (_template == null)
-                {
-                    Close();
-                    return;
-                }
+                    //Schematic Loading
+                    ErrorLogging.TelemetryClient?.TrackEvent(nameof(ImportTemplateSchematic));
+                    HouseGenSchematics.Add(ClipboardBuffer.Load(ofd.FileName));
 
-                GenHouse();
+                    //Template Loading
+                    string jsonValue = "";
+                    using (var sr = new StreamReader(new FileStream(Path.GetDirectoryName(ofd.FileName) + "\\" + filename + ".json", FileMode.Open, FileAccess.Read, FileShare.Read)))
+                    {
+                        jsonValue = sr.ReadToEnd();
+                    }
+
+                    HouseGenTemplateData data = JsonConvert.DeserializeObject<HouseGenTemplateData>(jsonValue);
+                    HouseGenTemplates.Add(new HouseGenTemplate(schematic_id, filename, data));
+
+
+                    System.Windows.Controls.ComboBoxItem cbi = new();
+                    cbi.Content = HouseGenTemplates[schematic_id].Name;
+                    SelectedTemplate.Items.Add(cbi);
+                    SelectedTemplate.SelectedItem = cbi;
+
+                }
+                else
+                {
+                    return false;
+                }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (HouseGenTemplates == null)
+            {
+                return false;
+            }
+            return true;
         }
 
         private void GenHouse()
         {
-            _buffer = new(_bufferSize);
+            
             Random rand = new();
 
-            Geometry.Primitives.Vector2Int32 roof1_Size = new(29, 5);
-            int roof1_OffsetX = 0;
-            int roof1_OffsetY = 0;
+            //Select house type
+            int houseType = rand.Next(HouseGenTemplates.Count);
 
-            Geometry.Primitives.Vector2Int32 roof2_Size = new(16, 5);
-            int roof2_OffsetX = 27;
-            int roof2_OffsetY = 7;
+            ClipboardBuffer templateSchematic = HouseGenSchematics[HouseGenTemplates[houseType].SchematicID];
+            HouseGenTemplateData templateData = HouseGenTemplates[houseType].Template;
 
-            Geometry.Primitives.Vector2Int32 room1_Size = new(25, 7);
-            int room1_OffsetX = 2;
-            int room1_OffsetY = 5;
+            //Retrive buffer size.
+            _bufferSize.X = templateSchematic.Size.X;
+            _bufferSize.Y = templateSchematic.Size.Y / templateData.Count;
 
-            Geometry.Primitives.Vector2Int32 room2_Size = new(24, 10);
-            int room2_OffsetX = 2;
-            int room2_OffsetY = 12;
+            _generatedSchematic = new(_bufferSize);
 
-            Geometry.Primitives.Vector2Int32 room3_Size = new(15, 10);
-            int room3_OffsetX = 26;
-            int room3_OffsetY = 12;
+            int type;
 
-            int roof1_Type = rand.Next(3);
-            int roof2_Type = roof1_Type;
-            int room1_Type = rand.Next(3);
-            int room2_Type = rand.Next(3);
-            int room3_Type = rand.Next(3);
+            //Process Rooms
+            for (int i = 0; i < templateData.Rooms.Count; i++)
+            {
+                type = rand.Next(templateData.Count);
+
+                for (int x = 0; x < templateData.Rooms[i].Width; x++)
+                {
+                    for (int y = 0; y < templateData.Rooms[i].Height; y++)
+                    {
+                        _generatedSchematic.Tiles[x + templateData.Rooms[i].X, y + templateData.Rooms[i].Y] = (Tile)templateSchematic.Tiles[x + templateData.Rooms[i].X, y + templateData.Rooms[i].Y + (_bufferSize.Y * type)].Clone();
+                    }
+                }
+            }
+
+            //Process Roofs
+            type = rand.Next(templateData.Count); 
+
+            for (int i = 0; i < templateData.Roofs.Count; i++)
+            {
+                for (int x = 0; x < templateData.Roofs[i].Width; x++)
+                {
+                    for (int y = 0; y < templateData.Roofs[i].Height; y++)
+                    {
+                        _generatedSchematic.Tiles[x + templateData.Roofs[i].X, y + templateData.Roofs[i].Y] = (Tile)templateSchematic.Tiles[x + templateData.Roofs[i].X, y + templateData.Roofs[i].Y + (_bufferSize.Y * type)].Clone();
+                    }
+                }
+            }
 
             byte roofColor = (byte)rand.Next(31);
 
-            for (int x = 0; x < roof1_Size.X; x++)
-            {
-                for (int y = 0; y < roof1_Size.Y; y++)
-                {
-                    _buffer.Tiles[x + roof1_OffsetX, y + roof1_OffsetY] = (Tile)_template.Tiles[x + roof1_OffsetX, y + roof1_OffsetY + (_bufferSize.Y * roof1_Type)].Clone();
-                    _buffer.Tiles[x + roof1_OffsetX, y + roof1_OffsetY].TileColor = roofColor;
-                }
-            }
-
-            for (int x = 0; x < roof2_Size.X; x++)
-            {
-                for (int y = 0; y < roof2_Size.Y; y++)
-                {
-                    _buffer.Tiles[x + roof2_OffsetX, y + roof2_OffsetY] = (Tile)_template.Tiles[x + roof2_OffsetX, y + roof2_OffsetY + (_bufferSize.Y * roof2_Type)].Clone();
-                    _buffer.Tiles[x + roof2_OffsetX, y + roof2_OffsetY].TileColor = roofColor;
-                }
-            }
-
-            for (int x = 0; x < room1_Size.X; x++)
-            {
-                for (int y = 0; y < room1_Size.Y; y++)
-                {
-                    _buffer.Tiles[x + room1_OffsetX, y + room1_OffsetY] = (Tile)_template.Tiles[x + room1_OffsetX, y + room1_OffsetY + (_bufferSize.Y * room1_Type)].Clone();
-                }
-            }
-
-            for (int x = 0; x < room2_Size.X; x++)
-            {
-                for (int y = 0; y < room2_Size.Y; y++)
-                {
-                    _buffer.Tiles[x + room2_OffsetX, y + room2_OffsetY] = (Tile)_template.Tiles[x + room2_OffsetX, y + room2_OffsetY + (_bufferSize.Y * room2_Type)].Clone();
-                }
-            }
-
-            for (int x = 0; x < room3_Size.X; x++)
-            {
-                for (int y = 0; y < room3_Size.Y; y++)
-                {
-                    _buffer.Tiles[x + room3_OffsetX, y + room3_OffsetY] = (Tile)_template.Tiles[x + room3_OffsetX, y + room3_OffsetY + (_bufferSize.Y * room3_Type)].Clone();
-                }
-            }
-
-            //Fill in any empty space.
+            //Fill in any empty space of schematic outside of room definintions (empty space within bounds of roof or room should alredy be filled)
             for (int x2 = 0; x2 < _bufferSize.X; x2++)
             {
                 for (int y2 = 0; y2 < _bufferSize.Y; y2++)
                 {
-                    if (_buffer.Tiles[x2, y2] == null) _buffer.Tiles[x2, y2] = new Tile();
+                    if (_generatedSchematic.Tiles[x2, y2] == null) _generatedSchematic.Tiles[x2, y2] = new Tile();
                 }
             }
 
-            _buffer.RenderBuffer();
             UpdatePreview();
-            PreviewImage.Source = _preview;
+            
         }
 
         private void UpdatePreview()
         {
-            _preview = _buffer.Preview;
+            _generatedSchematic.RenderBuffer();
+            PreviewImage.Source = _generatedSchematic.Preview;
             Copy.IsEnabled = true;
         }
 
         private void CancelButtonClick(object sender, RoutedEventArgs e)
         {
-            Copy.IsEnabled = false;
-            _buffer = new(_bufferSize);
+            Copy.IsEnabled = false;            
             Hide();
         }
 
         private void GenButtonClick(object sender, RoutedEventArgs e)
         {
-            LoadTemplateSchematic();
             GenHouse();
             UpdatePreview();
         }
 
         private void CopyButtonClick(object sender, RoutedEventArgs e)
         {
-            _wvm.Clipboard.LoadedBuffers.Add(_buffer);
-            Copy.IsEnabled = false;
+            _wvm.Clipboard.LoadedBuffers.Add(_generatedSchematic);
         }
 
-        private void HouseGenPreview_LoadContent(object sender, GraphicsDeviceEventArgs e)
+        private void ImportButtonClick(object sender, RoutedEventArgs e)
         {
-            /*
-            // Abort rendering if in design mode or if gameTimer is already running
-            if (ViewModelBase.IsInDesignModeStatic || _gameTimer.IsRunning)
+            if (ImportTemplateSchematic())
             {
-                return;
+                Generate.IsEnabled = true;
+                GenHouse();
             }
-            InitializeGraphicsComponents(e);
-
-
-            if (_textureDictionary.Valid)
-                LoadTerrariaTextures(e);
-
-            _selectionTexture = new Texture2D(e.GraphicsDevice, 1, 1);
-            LoadResourceTextures(e);
-
-            _selectionTexture.SetData(new[] { Color.FromNonPremultiplied(0, 128, 255, 128) }, 0, 1);
-            // Start the Game Timer
-            _gameTimer.Start();
-            */
-        }
-
-        private void HouseGenPreview_RenderXna(object sender, GraphicsDeviceEventArgs e)
-        {
-            /*
-            // Abort rendering if in design mode or if gameTimer is not running
-            if (!_gameTimer.IsRunning || _wvm.CurrentWorld == null || ViewModelBase.IsInDesignModeStatic)
-                return;
-
-            // Clear the graphics device and texture buffer
-            e.GraphicsDevice.Clear(_backgroundColor);
-
-
-
-            Update(e);
-            Render(e);
-            */
         }
     }
 }
