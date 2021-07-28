29c61fb7e7a5820ab292ce80ea367c7ab7278918
BinaryConstruct <binaryconstruct@gmail.com>
2021-07-28 10:49:26 -0500

    Version 4.5.1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 param(
-    [string] $VersionPrefix = "4.5.0",
-    [string] $VersionSuffix = "beta4"
+    [string] $VersionPrefix = "4.5.1",
+    [string] $VersionSuffix
 )
 
 $buildArgs = @(
@@ -10,12 +10,15 @@ $buildArgs = @(
     ".\src\TEdit.sln", 
     '/p:signcert="BC Code Signing"')
 
+$filename = "TEdit-$VersionPrefix"
+
 if ($null -ne $VersionPrefix) {
     $buildArgs += "/p:VersionPrefix=""$VersionPrefix"""
 }
 
 if ($null -ne $VersionSuffix) {
     $buildArgs += "/p:VersionSuffix=""$VersionSuffix"""
+    $filename = "TEdit-$VersionPrefix-$VersionSuffix"
 }
 
 & dotnet $buildArgs
@@ -26,15 +29,15 @@ Remove-Item -Path ".\TEdit*.msi"
 
 mkdir -Path ".\release"
 
-Copy-Item -Path ".\src\TEdit\bin\Release\net462\publish" -Destination ".\release\TEdit-$VersionPrefix-$VersionSuffix\" -Recurse -Force
+Copy-Item -Path ".\src\TEdit\bin\Release\net462\publish" -Destination ".\release\$filename\" -Recurse -Force
 Copy-Item -Path ".\schematics" -Destination ".\release" -Recurse
 
 # Create Installer
 $env:VERSION_PREFIX = $VersionPrefix
 $env:VERSION_SUFFIX = $VersionSuffix
 & dotnet build -c Release ".\src\Setup\Setup.csproj" 
-signtool.exe sign /v /fd sha256 /n "BC Code Signing" /t http://timestamp.digicert.com ".\src\Setup\TEdit-$VersionPrefix-$VersionSuffix.msi"
-Move-Item ".\src\Setup\TEdit-$VersionPrefix-$VersionSuffix.msi" ".\"
+signtool.exe sign /v /fd sha256 /n "BC Code Signing" /t http://timestamp.digicert.com ".\src\Setup\$filename.msi"
+Move-Item ".\src\Setup\$filename.msi" ".\"
 
 # Create ZIP Release
-Compress-Archive -Path ".\release\*" -DestinationPath ".\TEdit-$VersionPrefix-$VersionSuffix.zip"
+Compress-Archive -Path ".\release\*" -DestinationPath ".\$filename.zip"
