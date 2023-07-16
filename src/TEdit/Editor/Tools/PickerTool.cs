021a40633d608001a6c8b65a69cf98180ee50fd5
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2023-07-15 20:42:58 -0500

    Added liquid amount to picker tool.

--- src/TEdit/Editor/Tools/PickerTool.cs
+++ src/TEdit/Editor/Tools/PickerTool.cs
@@ -83,6 +83,32 @@ namespace TEdit.Editor.Tools
             {
                 _wvm.TilePicker.JunctionBoxMode = JunctionBoxMode.Right;
             }
+
+            // Get Picker For Liquid Amount.
+            if (curTile.LiquidAmount == 0)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.ZeroPercent;
+            else if (curTile.LiquidAmount <= 0x1A)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.TenPercent;
+            else if (curTile.LiquidAmount <= 0x33)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.TwentyPercent;
+            else if (curTile.LiquidAmount <= 0x4D)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.ThirtyPercent;
+            else if (curTile.LiquidAmount <= 0x66)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.FourtyPercent;
+            else if (curTile.LiquidAmount <= 0x80)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.FiftyPercent;
+            else if (curTile.LiquidAmount <= 0x99)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.SixtyPercent;
+            else if (curTile.LiquidAmount <= 0xB3)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.SeventyPercent;
+            else if (curTile.LiquidAmount <= 0xCC)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.EightyPercent;
+            else if (curTile.LiquidAmount <= 0xE6)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.NinteyPercent;
+            else if (curTile.LiquidAmount <= 0xFF)
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.OneHundredPercent;
+            else
+                _wvm.TilePicker.LiquidAmountMode = LiquidAmountMode.OneHundredPercent;
         }
 
         private void PickmaskTile(int x, int y)
