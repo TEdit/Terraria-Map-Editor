d275daf1d007aa8994c50c4da69d71a50e4c657a
BinaryConstruct <binaryconstruct@gmail.com>
2023-07-16 22:09:06 -0500

    notes

--- src/TEdit.Configuration/BestiaryData.cs
+++ src/TEdit.Configuration/BestiaryData.cs
@@ -3,74 +3,73 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace TEdit.Configuration
+namespace TEdit.Configuration;
+
+public class BestiaryNpcData
 {
-    public class NpcData
+    public int Id { get; set; }
+    public int BannerId { get; set; }
+    public string FullName { get; set; }
+    public string Name { get; set; }
+    public string BestiaryId { get; set; }
+    public bool CanTalk { get; set; }
+    public bool IsCritter { get; set; }
+    public bool IsTownNpc { get; set; }
+    public bool IsKillCredit { get; set; }
+    public int BestiaryDisplayIndex { get; set; }
+}
+
+public class BestiaryNpcConfiguration
+{
+    public List<string> Cat { get; private set; } = new();
+    public List<string> Dog { get; private set; } = new();
+    public List<string> Bunny { get; private set; } = new();
+    public List<BestiaryNpcData> NpcData { get; private set; } = new();
+}
+
+
+public class BestiaryData
+{
+    public BestiaryNpcConfiguration Configuration { get; private set; } = new();
+
+    public Dictionary<int, BestiaryNpcData> NpcById { get; private set; } = new();
+    public Dictionary<string, BestiaryNpcData> NpcData { get; private set; } = new();
+
+    public List<string> BestiaryTalkedIDs { get; private set; } = new();
+    public List<string> BestiaryNearIDs { get; private set; } = new();
+    public List<string> BestiaryKilledIDs { get; private set; } = new();
+
+    public static BestiaryData LoadJson(string fileName)
     {
-        public int Id { get; set; }
-        public int BannerId { get; set; }
-        public string FullName { get; set; }
-        public string Name { get; set; }
-        public string BestiaryId { get; set; }
-        public bool CanTalk { get; set; }
-        public bool IsCritter { get; set; }
-        public bool IsTownNpc { get; set; }
-        public bool IsKillCredit { get; set; }
-        public int BestiaryDisplayIndex { get; set; }
+        using (StreamReader file = File.OpenText(fileName))
+        using (JsonTextReader reader = new JsonTextReader(file))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            var npcConfig = serializer.Deserialize<BestiaryNpcConfiguration>(reader);
+            var bestiaryData = new BestiaryData();
+            bestiaryData.Init(npcConfig);
+
+            return bestiaryData;
+        }
     }
 
-    public class NpcConfiguration
+    private void Init(BestiaryNpcConfiguration npcConfig)
     {
-        public List<string> Cat { get; private set; } = new();
-        public List<string> Dog { get; private set; } = new();
-        public List<string> Bunny { get; private set; } = new();
-        public List<NpcData> NpcData { get; private set; } = new();
-    }
-
-
-    public class BestiaryData
-    {
-        public NpcConfiguration Configuration { get; private set; } = new();
-
-        public Dictionary<int, NpcData> NpcById { get; private set; } = new();
-        public Dictionary<string, NpcData> NpcData { get; private set; } = new();
-
-        public List<string> BestiaryTalkedIDs { get; private set; } = new();
-        public List<string> BestiaryNearIDs { get; private set; } = new();
-        public List<string> BestiaryKilledIDs { get; private set; } = new();
-
-        public static BestiaryData LoadJson(string fileName)
+        Configuration = npcConfig;
+        NpcData.Clear();
+        foreach (var npc in npcConfig.NpcData)
         {
-            using (StreamReader file = File.OpenText(fileName))
-            using (JsonTextReader reader = new JsonTextReader(file))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                var npcConfig = serializer.Deserialize<NpcConfiguration>(reader);
-                var bestiaryData = new BestiaryData();
-                bestiaryData.Init(npcConfig);
-
-                return bestiaryData;
-            }
+            NpcById[npc.Id] = npc;
+            NpcData[npc.BestiaryId] = npc;
         }
 
-        private void Init(NpcConfiguration npcConfig)
-        {
-            Configuration = npcConfig;
-            NpcData.Clear();
-            foreach (var npc in npcConfig.NpcData)
-            {
-                NpcById[npc.Id] = npc;
-                NpcData[npc.BestiaryId] = npc;
-            }
+        BestiaryTalkedIDs.Clear();
+        BestiaryTalkedIDs.AddRange(NpcData.Where(npc => npc.Value.CanTalk).Select(npc => npc.Value.BestiaryId));
 
-            BestiaryTalkedIDs.Clear();
-            BestiaryTalkedIDs.AddRange(NpcData.Where(npc => npc.Value.CanTalk).Select(npc => npc.Value.BestiaryId));
+        BestiaryNearIDs.Clear();
+        BestiaryNearIDs.AddRange(NpcData.Where(npc => npc.Value.IsCritter).Select(npc => npc.Value.BestiaryId));
 
-            BestiaryNearIDs.Clear();
-            BestiaryNearIDs.AddRange(NpcData.Where(npc => npc.Value.IsCritter).Select(npc => npc.Value.BestiaryId));
-
-            BestiaryKilledIDs.Clear();
-            BestiaryKilledIDs.AddRange(NpcData.Where(npc => npc.Value.IsKillCredit).Select(npc => npc.Value.BestiaryId));
-        }
+        BestiaryKilledIDs.Clear();
+        BestiaryKilledIDs.AddRange(NpcData.Where(npc => npc.Value.IsKillCredit).Select(npc => npc.Value.BestiaryId));
     }
 }
