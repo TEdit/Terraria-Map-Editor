fd8373429577838143f18d06659d8ffdaa57afb4
BinaryConstruct <binaryconstruct@gmail.com>
2024-07-03 22:50:59 -0500

    Add tool menus

--- src/TEdit.Desktop/App.axaml.cs
+++ src/TEdit.Desktop/App.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using TEdit.Desktop.Controls.WorldRenderEngine;
+using TEdit.Desktop.Editor;
 using TEdit.Desktop.Services;
 using TEdit.Desktop.ViewModels;
 using TEdit.Desktop.Views;
@@ -12,24 +13,52 @@ namespace TEdit.Desktop;
 
 public partial class App : Application
 {
+    public IServiceProvider Services { get; private set; }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
 
         var services = new ServiceCollection();
+
+        // register view models
         services.AddTransient<MainWindowViewModel>();
+        services.AddTransient<DocumentViewModel>();
+        services.AddTransient<FileManagerViewModel>();
+
+        // register editing tools
+        services.AddSingleton<ToolSelectionViewModel>();
+        services.AddSingleton<IMouseTool, ArrowTool>();
+        services.AddSingleton<IMouseTool, BrushTool>();
+        services.AddSingleton<IMouseTool, PencilTool>();
+        services.AddSingleton<IMouseTool, SelectTool>();
+
+        // register services
         services.AddTransient<IDialogService, DialogService>();
 
         //services.AddSingleton<IMyInterface, MyImplementation>()
         var serviceProvider = services.BuildServiceProvider();
         this.Resources[typeof(IServiceProvider)] = serviceProvider;
+        Services = serviceProvider;
     }
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var services = (IServiceProvider)this.Resources[typeof(IServiceProvider)];
+        var vm = services.GetRequiredService<MainWindowViewModel>();
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = vm
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainWindow
+            {
+                DataContext = vm
+            };
         }
 
         base.OnFrameworkInitializationCompleted();
---
+++ src/TEdit.Desktop/Views/PaintModeView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace TEdit.Desktop.Views
+{
+    public partial class PaintModeView : UserControl
+    {
+        public PaintModeView()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
