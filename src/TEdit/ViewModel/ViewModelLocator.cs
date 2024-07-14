6f8571fddfff5d09e11236c504a12ce7b29d1366
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2024-07-13 21:22:03 -0500

    Added an Image to Pixel-art Editor Plugin. (#1910)
    * Added an Image to Pixel-art Editor Plugin.
    
    * Added an Image to Pixel-art Editor Plugin.
    
    * Added an Image to Pixel-art Editor Plugin.

--- src/TEdit/ViewModel/ViewModelLocator.cs
+++ src/TEdit/ViewModel/ViewModelLocator.cs
@@ -47,20 +47,21 @@ public static class ViewModelLocator
         wvm.ActiveTool = defaultTool;
 
         //Sorted by Plugin-Name
+        wvm.Plugins.Add(new BlockShufflePlugin(wvm));
         wvm.Plugins.Add(new FindChestWithPlugin(wvm));
         wvm.Plugins.Add(new FindPlanteraBulbPlugin(wvm));
         wvm.Plugins.Add(new FindTileWithPlugin(wvm));
-        wvm.Plugins.Add(new SpriteDebuggerPlugin(wvm));
-        wvm.Plugins.Add(new TextStatuePlugin(wvm));
         wvm.Plugins.Add(new HouseGenPlugin(wvm));
+        wvm.Plugins.Add(new ImageToPixelartEditor(wvm));
         wvm.Plugins.Add(new RandomizerPlugin(wvm));
         wvm.Plugins.Add(new RemoveAllChestsPlugin(wvm));
         wvm.Plugins.Add(new RemoveAllUnlockedChestsPlugin(wvm));
         wvm.Plugins.Add(new RemoveTileWithPlugin(wvm));
         wvm.Plugins.Add(new ReplaceAllPlugin(wvm));
         wvm.Plugins.Add(new SandSettlePlugin(wvm));
-        wvm.Plugins.Add(new BlockShufflePlugin(wvm));
         wvm.Plugins.Add(new SimplePerlinGeneratorPlugin(wvm));
+        wvm.Plugins.Add(new SpriteDebuggerPlugin(wvm));
+        wvm.Plugins.Add(new TextStatuePlugin(wvm));
         wvm.Plugins.Add(new UnlockAllChestsPlugin(wvm));
         return wvm;
     }
