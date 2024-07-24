fa9b14ce022b2312b31be5f2177ff0b72d29c269
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2024-07-23 23:02:45 -0500

    Fixed replacing tiles to air vise versa crashing.

--- src/TEdit/Editor/Plugins/ReplaceAllPlugin.cs
+++ src/TEdit/Editor/Plugins/ReplaceAllPlugin.cs
@@ -4,136 +4,151 @@ using TEdit.Render;
 using TEdit.Terraria;
 using TEdit.ViewModel;
 
-namespace TEdit.Editor.Plugins;
-
-public class ReplaceAllPlugin : BasePlugin
+namespace TEdit.Editor.Plugins
 {
-    public ReplaceAllPlugin(WorldViewModel worldViewModel)
-        : base(worldViewModel)
+    public class ReplaceAllPlugin : BasePlugin
     {
-        Name = "Replace All Tiles";
-    }
-
-
-    private void PerformReplace()
-    {
-        if (_wvm.CurrentWorld == null)
-            return;
-
-        bool replaceTiles = false;
-        bool replaceWalls = false;
-
-
-        if (_wvm.TilePicker.PaintMode == PaintMode.TileAndWall)
+        public ReplaceAllPlugin(WorldViewModel worldViewModel)
+            : base(worldViewModel)
         {
-            if (_wvm.TilePicker.TileStyleActive)
-                replaceTiles = true;
-            if (_wvm.TilePicker.WallStyleActive)
-                replaceWalls = true;
+            Name = "Replace All Tiles";
         }
 
-        if (replaceTiles && _wvm.TilePicker.TileMaskMode == MaskMode.Off)
+        private void PerformReplace()
         {
-            MessageBox.Show("Enable masking tiles to enable replace.");
-            return;
-        }
+            if (_wvm.CurrentWorld == null)
+                return;
 
-        if (replaceWalls && _wvm.TilePicker.WallMaskMode == MaskMode.Off)
-        {
-            MessageBox.Show("Enable masking walls to enable replace.");
-            return;
-        }
+            bool replaceTiles = false;
+            bool replaceWalls = false;
 
-        int wallMask = _wvm.TilePicker.WallMask;
-        int tileMask = _wvm.TilePicker.TileMask;
-        int tileTarget = _wvm.TilePicker.Tile;
-        int wallTarget = _wvm.TilePicker.Wall;
-
-        for (int x = (_wvm.Selection.IsActive) ? _wvm.Selection.SelectionArea.X : 0; x < ((_wvm.Selection.IsActive) ? _wvm.Selection.SelectionArea.X + _wvm.Selection.SelectionArea.Width : _wvm.CurrentWorld.TilesWide); x++)
-        {
-            for (int y = (_wvm.Selection.IsActive) ? _wvm.Selection.SelectionArea.Y : 0; y < ((_wvm.Selection.IsActive) ? _wvm.Selection.SelectionArea.Y + _wvm.Selection.SelectionArea.Height : _wvm.CurrentWorld.TilesHigh); y++)
+            if (_wvm.TilePicker.PaintMode == PaintMode.TileAndWall)
             {
-                bool doReplaceTile = false;
-                bool doReplaceWall = false;
+                if (_wvm.TilePicker.TileStyleActive)
+                    replaceTiles = true;
+                if (_wvm.TilePicker.WallStyleActive)
+                    replaceWalls = true;
+            }
 
-                Tile curTile = _wvm.CurrentWorld.Tiles[x, y];
+            if (replaceTiles && _wvm.TilePicker.TileMaskMode == MaskMode.Off)
+            {
+                MessageBox.Show("Enable masking tiles to enable replace.");
+                return;
+            }
 
+            if (replaceWalls && _wvm.TilePicker.WallMaskMode == MaskMode.Off)
+            {
+                MessageBox.Show("Enable masking walls to enable replace.");
+                return;
+            }
 
-                if (replaceTiles)
+            int wallMask = _wvm.TilePicker.WallMask;
+            int tileMask = _wvm.TilePicker.TileMask;
+            int tileTarget = _wvm.TilePicker.Tile;
+            int wallTarget = _wvm.TilePicker.Wall;
+
+            for (int x = (_wvm.Selection.IsActive) ? _wvm.Selection.SelectionArea.X : 0; x < ((_wvm.Selection.IsActive) ? _wvm.Selection.SelectionArea.X + _wvm.Selection.SelectionArea.Width : _wvm.CurrentWorld.TilesWide); x++)
+            {
+                for (int y = (_wvm.Selection.IsActive) ? _wvm.Selection.SelectionArea.Y : 0; y < ((_wvm.Selection.IsActive) ? _wvm.Selection.SelectionArea.Y + _wvm.Selection.SelectionArea.Height : _wvm.CurrentWorld.TilesHigh); y++)
                 {
-                    if ((_wvm.Selection.IsValid(x,y)) && (curTile.IsActive && curTile.Type == tileMask && _wvm.TilePicker.TileMaskMode == MaskMode.Match)
-                        || (!curTile.IsActive && _wvm.TilePicker.TileMaskMode == MaskMode.Empty)
-                        || (curTile.Type != tileMask && _wvm.TilePicker.TileMaskMode == MaskMode.NotMatching))
-                    {
-                        doReplaceTile = true;
-                    }
-                }
+                    bool doReplaceTile = false;
+                    bool doReplaceWall = false;
 
-                if (replaceWalls)
-                {
-                    if ((_wvm.Selection.IsValid(x, y)) && (curTile.Wall == wallMask && _wvm.TilePicker.WallMaskMode == MaskMode.Match)
-                        || (curTile.Wall == 0 && _wvm.TilePicker.WallMaskMode == MaskMode.Empty)
-                        || (curTile.Wall != wallMask && _wvm.TilePicker.WallMaskMode == MaskMode.NotMatching))
+                    Tile curTile = _wvm.CurrentWorld.Tiles[x, y];
+					
+					// Updated condition to handle air tiles (tileMask == -1).
+                    if (replaceTiles)
                     {
-                        doReplaceWall = true;
-                    }
-                }
-
-                if (doReplaceTile || doReplaceWall)
-                {
-                    _wvm.UndoManager.SaveTile(x, y);
-
-                    if (doReplaceTile)
-                    {
-                        curTile.Type = (ushort)tileTarget;
-                        curTile.IsActive = true;
-                        
-                        if (WorldConfiguration.TileProperties[curTile.Type].IsSolid)
+                        if ((_wvm.Selection.IsValid(x, y)) && ((curTile.IsActive && curTile.Type == tileMask && _wvm.TilePicker.TileMaskMode == MaskMode.Match)
+                            || (!curTile.IsActive && _wvm.TilePicker.TileMaskMode == MaskMode.Empty)
+                            || (curTile.Type != tileMask && _wvm.TilePicker.TileMaskMode == MaskMode.NotMatching)
+                            || (tileMask == -1 && !curTile.IsActive))) // Added condition to handle tileMask == -1.
                         {
-                            curTile.U = -1;
-                            curTile.V = -1;
-                            BlendRules.ResetUVCache(_wvm, x, y, 1, 1);
-                        }
-
-                        if (_wvm.TilePicker.TilePaintActive)
-                        {
-                            curTile.TileColor = (byte)_wvm.TilePicker.TileColor;
+                            doReplaceTile = true;
                         }
                     }
 
-                    if (doReplaceWall)
+                    if (replaceWalls)
                     {
-                        curTile.Wall = (byte)wallTarget;
-                        
-                        if (_wvm.TilePicker.WallPaintActive)
+                        if ((_wvm.Selection.IsValid(x, y)) && (curTile.Wall == wallMask && _wvm.TilePicker.WallMaskMode == MaskMode.Match)
+                            || (curTile.Wall == 0 && _wvm.TilePicker.WallMaskMode == MaskMode.Empty)
+                            || (curTile.Wall != wallMask && _wvm.TilePicker.WallMaskMode == MaskMode.NotMatching))
                         {
-                            if (curTile.Wall != 0)
+                            doReplaceWall = true;
+                        }
+                    }
+
+                    if (doReplaceTile || doReplaceWall)
+                    {
+                        _wvm.UndoManager.SaveTile(x, y);
+
+                        if (doReplaceTile)
+                        {
+                            if (tileTarget == -1)
                             {
-                                curTile.WallColor = (byte)_wvm.TilePicker.WallColor;
+								// If tileTarget is -1, make the tile inactive.
+                                curTile.IsActive = false;
                             }
                             else
                             {
-                                curTile.WallColor = (byte)0;
+                                curTile.Type = (ushort)tileTarget;
+                                curTile.IsActive = true;
+
+                                if (WorldConfiguration.TileProperties[curTile.Type].IsSolid)
+                                {
+                                    curTile.U = -1;
+                                    curTile.V = -1;
+                                    BlendRules.ResetUVCache(_wvm, x, y, 1, 1);
+                                }
+
+                                if (_wvm.TilePicker.TilePaintActive)
+                                {
+                                    curTile.TileColor = (byte)_wvm.TilePicker.TileColor;
+                                }
                             }
                         }
-                    }
 
-                    _wvm.UpdateRenderPixel(x, y);
+                        if (doReplaceWall)
+                        {
+                            if (wallTarget == -1)
+                            {
+                                curTile.Wall = 0;
+                            }
+                            else
+                            {
+                                curTile.Wall = (byte)wallTarget;
+
+                                if (_wvm.TilePicker.WallPaintActive)
+                                {
+                                    if (curTile.Wall != 0)
+                                    {
+                                        curTile.WallColor = (byte)_wvm.TilePicker.WallColor;
+                                    }
+                                    else
+                                    {
+                                        curTile.WallColor = (byte)0;
+                                    }
+                                }
+                            }
+                        }
+
+                        _wvm.UpdateRenderPixel(x, y);
+                    }
                 }
             }
+
+            _wvm.UndoManager.SaveUndo();
         }
 
-        _wvm.UndoManager.SaveUndo();
-    }
-
-    public override void Execute()
-    {
-        ReplaceAllPluginView view = new ReplaceAllPluginView();
-        view.Owner = Application.Current.MainWindow;
-        view.DataContext = _wvm;
-        if (view.ShowDialog() == true)
+        public override void Execute()
         {
-            PerformReplace();
+            ReplaceAllPluginView view = new ReplaceAllPluginView();
+            view.Owner = Application.Current.MainWindow;
+            view.DataContext = _wvm;
+            if (view.ShowDialog() == true)
+            {
+                PerformReplace();
+            }
         }
     }
 }
