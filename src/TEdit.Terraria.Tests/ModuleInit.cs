39521e593120c4007034a06fbaccf911d28ddd3f
BinaryConstruct <binaryconstruct@gmail.com>
2026-02-21 19:26:30 -0600

    Add ReactiveUI v23 module initializer for test project

---
+++ src/TEdit.Terraria.Tests/ModuleInit.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+using ReactiveUI.Builder;
+
+namespace TEdit.Terraria.Tests;
+
+internal static class ModuleInit
+{
+    [ModuleInitializer]
+    internal static void Initialize()
+    {
+        RxAppBuilder.CreateReactiveUIBuilder().Build();
+    }
+}
